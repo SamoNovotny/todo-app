cf2644eae20913d7f365f4da7996acc667423246
Samuel Novotný <samuel.novotny@student.tuke.sk>
2022-09-19 18:53:52 +0200

    Updates

---
+++ precache-manifest.f2374653505651381aa525901f8a0880.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ddba0d0ed689aa8809e",
+    "url": "/todo-app/static/js/main.6ddba0d0.chunk.js"
+  },
+  {
+    "revision": "497dc6329578ae794410",
+    "url": "/todo-app/static/js/1.497dc632.chunk.js"
+  },
+  {
+    "revision": "fd450241c90af22dc3c6",
+    "url": "/todo-app/static/js/2.fd450241.chunk.js"
+  },
+  {
+    "revision": "0401336c3f599bcc3634",
+    "url": "/todo-app/static/js/runtime~main.0401336c.js"
+  },
+  {
+    "revision": "f6a269d60a306167c24d5aa9d3e5dd2b",
+    "url": "/todo-app/index.html"
+  }
+];
